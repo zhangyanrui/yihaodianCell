fffef341d0c475d16fc31022fc5d3ed5a05f0723
Will <zhangyanrui@sina.cn>
2014-07-16 17:41:42 +0800

    Initial Commit

---
+++ TestTableViewCellZooming/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestTableViewCellZooming
+//
+//  Created by Will on 14-7-16.
+//  Copyright (c) 2014年 Will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestTableViewCellZooming/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestTableViewCellZooming
+//
+//  Created by Will on 14-7-16.
+//  Copyright (c) 2014年 Will. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
